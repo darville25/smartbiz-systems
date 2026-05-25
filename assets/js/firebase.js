f13f6460eec9892621e59ddcb26fab4ccf2a88d1
darville <josephkenindia@gmail.com>
2026-05-25 12:24:47 +0300

    Add files via upload
    newly uploaded site

---
+++ assets/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCk2HaQB_nMpoSJ_w9tE-wvkZXJBnq7MAc",
+  authDomain: "smartbizsystem-2111a.firebaseapp.com",
+  projectId: "smartbizsystem-2111a",
+  storageBucket: "smartbizsystem-2111a.firebasestorage.app",
+  messagingSenderId: "663381507458",
+  appId: "1:663381507458:web:18fd6bbcc6b8aba772639b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
